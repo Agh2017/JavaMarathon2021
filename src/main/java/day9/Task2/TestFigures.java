26a7529d067f1f1abbfe1f7ab68e1473ffdd28e7
Agh2017 <agh2017@yandex.ru>
2021-07-31 22:31:17 +0300

    added solution day 9

--- src/main/java/day9/Task2/TestFigures.java
+++ src/main/java/day9/Task2/TestFigures.java
@@ -1,7 +1,45 @@
 package day9.Task2;
 
+
 public class TestFigures {
     public static void main(String[] args) {
 
+        Figure[] figures = {
+                new Triangle(10, 10, 10, "Red"),
+                new Triangle(10, 20, 30, "Green"),
+                new Triangle(10, 20, 15, "Red"),
+                new Rectangle(5, 10, "Red"),
+                new Rectangle(40, 15, "Orange"),
+                new Circle(4, "Red"),
+                new Circle(10, "Red"),
+                new Circle(5, "Blue")
+        };
+
+        System.out.println(calculateRedPerimeter(figures));
+        System.out.println(calculateRedArea(figures));
+    }
+
+
+    public static double calculateRedPerimeter(Figure[] figures) {
+
+        double sumRedPerimeters = 0;
+        for (Figure figure : figures) {
+            if (figure.getColor().equals("Red")) {
+                sumRedPerimeters += figure.perimeter();
+            }
+        }
+        return sumRedPerimeters;
+    }
+
+    public static double calculateRedArea(Figure[] figures) {
+
+        double sumRedAreas = 0;
+        for (Figure figure : figures) {
+            if (figure.getColor().equals("Red")) {
+                sumRedAreas += figure.area();
+            }
+        }
+        return sumRedAreas;
     }
 }
+
