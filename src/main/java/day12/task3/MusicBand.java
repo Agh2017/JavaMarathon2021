15b2ba88c09d182e59b2aca29fee64bed8b594e3
Agh2017 <agh2017@yandex.ru>
2021-08-20 20:50:18 +0300

    added solution 1 day 14

--- src/main/java/day12/task3/MusicBand.java
+++ src/main/java/day12/task3/MusicBand.java
@@ -1,29 +1,43 @@
 package day12.task3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MusicBand {
 
+    private static final int AMOUNT = 11;
+    public static List<MusicBand> musicBands = new ArrayList<>();
     private String name;
     private int year;
+
+
 
     public MusicBand(String name, int year) {
         this.name = name;
         this.year = year;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setYear(int year) {
-        this.year = year;
-    }
-
-    public String getName() {
-        return name;
+    public static Object getBands() {
+        return musicBands;
     }
 
     public int getYear() {
         return year;
+    }
+
+    public static void makeBands() {
+        String[] groupList = {"Aerosmith", "1973", "Ace of Base", "1992", "Backstreet Boys", "1993", "Bad Boys Blue", "1984", "Gorg", "2020", "Hi-Fi", "1998", "Kamelot", "1991", "Pink Floyd", "1965", "Ленинград", "1996", "Airbourne", "2003", "Cellar Darling", "2016"};
+
+        for (int i=0, j=0; i<AMOUNT-1; i++, j+=2) {
+            int year = Integer.parseInt (groupList[j+1]);
+            MusicBand band = new MusicBand(groupList[j], year);
+            musicBands.add(i,band);
+        }
+    }
+
+    public static void shuffleGroupList() {
+        Collections.shuffle(musicBands);
     }
 
     @Override
--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -1,7 +1,28 @@
 package day14;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Task1 {
     public static void main(String[] args) {
+        try {
+            /*String separator = File.separator;
+            String path = separator + "C:" + separator + "JavaMarathon2021" + separator + "file1.txt";
+            */
+            String path = "file1.txt";
+            File file = new File(path);
+            printSumDigits(file);
+        } catch (FileNotFoundException e) {
+            System.out.print("Файл не найден");;
+        }
 
+    }
+
+    public static void printSumDigits(File file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(file);
+        String input = scanner.nextLine();
+        String[] dataFile1 = input.split(" ");
+        System.out.println(Arrays.toString(dataFile1));
     }
 }
