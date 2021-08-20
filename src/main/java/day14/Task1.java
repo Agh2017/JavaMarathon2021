dd78d489242f336690b8ff6d7b036956b27df67f
fantom <Agh2017@yandex.ru>
2021-08-20 22:34:52 +0300

    added task1 of day 14

--- src/main/java/day14/Task1.java
+++ src/main/java/day14/Task1.java
@@ -2,6 +2,7 @@ package day14;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Task1 {
@@ -14,15 +15,32 @@ public class Task1 {
             File file = new File(path);
             printSumDigits(file);
         } catch (FileNotFoundException e) {
-            System.out.print("Файл не найден");;
+            System.out.println("Файл не найден");
         }
-
     }
 
-    public static void printSumDigits(File file) throws FileNotFoundException {
-        Scanner scanner = new Scanner(file);
-        String input = scanner.nextLine();
-        String[] dataFile1 = input.split(" ");
-        System.out.println(Arrays.toString(dataFile1));
+    public static void printSumDigits(File file) throws FileNotFoundException, ArrayIndexOutOfBoundsException {
+        try {
+            Scanner scanner = new Scanner(file);
+            String input = scanner.nextLine();
+            String[] dataFile1 = input.split(" ");
+            if (dataFile1.length != 10) {
+                throw new ArrayIndexOutOfBoundsException(dataFile1.length);
+            } else {
+                int sum = 0;
+                for (String number : dataFile1) {
+                    sum += Integer.parseInt(number);
+                }
+                System.out.println(sum);
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Некорректный входной файл");
+        }
     }
 }
+/*
+Создать .txt файл в папке проекта, как показано в видео (урок 36, время 15:30). Заполнить его вручную десятью произвольными
+числами. Реализовать статический метод printSumDigits(File file), который считает сумму всех чисел в этом файле и выводит ее на экран.
+Если файла не существует в папке проекта, в программе необходимо выбрасывать исключение и выводить в консоль сообщение “Файл не найден”. Помимо этого, если чисел в файле меньше или больше 10, необходимо выбрасывать исключение и выводить в консоль сообщение “Некорректный входной файл”.
+Вызвать данный метод в методе main().
+ */
