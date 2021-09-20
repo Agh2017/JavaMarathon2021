1cf48ade990e2a76fa7b6ee01e1d26a34e31f519
fantom <Agh2017@yandex.ru>
2021-09-21 00:34:27 +0300

    added solution 1 of day 18

--- src/main/java/day18/Task1.java
+++ src/main/java/day18/Task1.java
@@ -1,7 +1,27 @@
 package day18;
+/*
+ Вам дан числовой массив произвольного размера. Посчитайте сумму чисел в этом массиве не используя циклы (необходимо использовать рекурсивные вызовы).
 
+Пример:
+int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+System.out.println(recursionSum(...)); // 65346
+
+*recursionSum(...) - это ваш рекурсивный метод, который вернет сумму чисел в массиве numbers.
+
+ */
 public class Task1 {
-    public static void main(String[] args) {
 
+    private static int count;
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 10, 1241, 50402, -50, 249, 10215, 665, 2295, 7, 311};
+        count = numbers.length-1;
+        System.out.println(recursionSum(numbers, count));
+    }
+
+    public static int recursionSum(int[] array, int count) {
+        if (count == -1) return 0;
+        int sum = array[count]+recursionSum(array, count-1);
+        return sum;
     }
 }
