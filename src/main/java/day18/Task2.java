fc2801a0dee68a202e8679c40d7c3c594e9afa06
Agh2017 <agh2017@yandex.ru>
2021-09-22 22:40:59 +0300

    added solution 2 day 18

--- src/main/java/day18/Task2.java
+++ src/main/java/day18/Task2.java
@@ -1,7 +1,17 @@
 package day18;
 
 public class Task2 {
-    public static void main(String[] args) {
 
+    private static final int x = 717771237;
+    private static final int reference = 7;
+    private static int count;
+
+    public static void main(String[] args) {
+        System.out.println(count7(x));
+    }
+
+    public static int count7(int n) {
+        if (n % 10 == reference) count++;
+        return n<1 ? count: count7(n / 10);
     }
 }
