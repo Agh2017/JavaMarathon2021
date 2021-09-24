8a39d1b503fed463cc0dc42e40f980b36042b5be
Agh2017 <Agh2017@yandex.ru>
2021-09-24 14:52:52 +0300

    current work d 18

---
+++ src/main/java/day18/Node.java
@@ -0,0 +1,15 @@
+package day18;
+
+public class Node {
+    private static int value;
+
+    public Node (int value) {
+        this.value=value;
+    }
+
+    Node root = new Node(20);
+
+    public static void add(Node node, int value) {
+
+    }
+}
